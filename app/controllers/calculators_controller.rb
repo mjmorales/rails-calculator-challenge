afc18ddc76a61ebfaee3c4822b26e9018a144c09
Manuel Morales <morales.jmanuel16@gmail.com>
2017-11-05 15:48:55 -0500

    created calculator input foundation

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -4,4 +4,12 @@ class CalculatorsController < ApplicationController
       format.html
     end
   end
+
+  def calculate
+    # TODO: Create module for expression calculation
+    response = 42
+    respond_to do |format|
+      format.json { render(json: response) }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'calculators#index'
+  resources :calculators, only: :index do
+    post :calculate, on: :collection
+    get :calculate, on: :collection
+  end
 end
