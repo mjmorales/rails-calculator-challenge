ce2f0a85af47e12b6715ccd0620a48468625dd6b
Manuel Morales <morales.jmanuel16@gmail.com>
2017-11-06 20:04:06 -0500

    add calculator dependency

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,3 +1,4 @@
+require 'calculator'
 class CalculatorsController < ApplicationController
   def index
     respond_to do |format|
@@ -6,10 +7,26 @@ class CalculatorsController < ApplicationController
   end
 
   def calculate
-    # TODO: Create module for expression calculation
-    response = 42
+    response = Calculator.new(calculator_params[:expression]).safe_eval
     respond_to do |format|
-      format.json { render(json: response) }
+      format.json { render(json: calculation_response_hash(response)) }
     end
   end
+
+  private
+
+  def calculator_params
+    # Lock down input with strong params
+    params.permit(:expression)
+  end
+
+  def calculation_response_hash(response)
+    # For more easily understood response messages by the user
+    # This response hash is redundant if treated as an API Endpoint
+    json_response = {}
+    json_response[:success] = response[:success] if response[:success]
+    json_response[:error] = 'There was an error please retry again.' if response[:errors]
+    json_response
+  end
+
 end
